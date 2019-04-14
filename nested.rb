749c74f3779c4833341bbd0f1f033b70175b01a9
Janice Pena <jipena94@gmail.com>
2019-04-14 22:48:07 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] = "C"
+    
 end
 
 def adding_matz
@@ -66,9 +71,15 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP", "C"]
+ 		    
+ 		  },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
+          
         },
         :alan_kay => {
           :known_for => "Object Orientation",
@@ -79,7 +90,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
     
 end
 
@@ -101,8 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +131,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] = ["C" , "Assembly"]
+programmer_hash
 
 end
